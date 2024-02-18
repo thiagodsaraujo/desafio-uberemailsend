b5413e2058d5e7571df22f089272d05c6e265aa4
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2024-02-18 14:59:53 -0300

    initial commit

---
+++ src/main/java/com/emailserviceuber/EmailServiceUberApplication.java
@@ -0,0 +1,13 @@
+package com.emailserviceuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceUberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceUberApplication.class, args);
+    }
+
+}
